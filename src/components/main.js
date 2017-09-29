ffc0a18ca64d73312203f5b0e8c031a9b2a60c30
Jorge Contreras <jorgeacontrerasl@gmail.com>
2017-09-29 16:40:27 -0700

    Initial commit, create a working Main component

---
+++ src/components/main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+
+class Main extends Component {
+  render(){
+    return(
+      <div id='main'>
+        <h1> Jorge Contreras-Loreto </h1>
+      </div>
+    );
+  }
+}
+
+export default Main;
